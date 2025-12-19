3be2c6527d54fc19978429d4a03bc72b87b99047
lucien144 <honza@lucien144.net>
2025-12-19 13:35:30 +0100

    fix: links and copy

--- src/components/ScreenshotsSection.vue
+++ src/components/ScreenshotsSection.vue
@@ -115,8 +115,8 @@ function onSwiper(_swiper) {
                 </svg>
                 Stát se patronem
               </a>
-              <p class="italic text-sm dark:text-gray-400">Do poznámky dejte svoje NYX ID. Do
-                několika hodin vám zapnu premim funkce. Každá další platba bude již spárovaná
+              <p class="italic text-sm dark:text-gray-400">Na Nyxu mi napište do soukromé zprávy vaše NYX ID.<br> Do
+                pár hodin vám zapnu premim funkce. Každá další platba bude již spárovaná
                 automaticky a premium automaticky obnoveno.</p>
             </div>
           </div>
@@ -165,7 +165,7 @@ function onSwiper(_swiper) {
             <div class="flex flex-col items-center gap-y-4">
               <p class="text-sm dark:text-gray-300">Minimálně na 6/12 měsíců, manuální zapnutí.</p>
               <div class="flex gap-x-2">
-                <a href="#"
+                <a href="@/assets/qr-420.jpg" target="_blank"
                    class="inline-flex items-center bg-black dark:bg-white dark:text-black rounded-lg text-white p-3 hover:bg-gray-800 dark:hover:bg-gray-100 transition">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -182,7 +182,7 @@ function onSwiper(_swiper) {
                   </svg>
                   420,-
                 </a>
-                <a href="#"
+                <a href="@/assets/qr-840.jpg" target="_blank"
                    class="inline-flex items-center bg-black dark:bg-white dark:text-black rounded-lg text-white p-3 hover:bg-gray-800 dark:hover:bg-gray-100 transition">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
